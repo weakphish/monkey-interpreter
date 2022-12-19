81ffee14278c15c5c0b043f7dc5be611e3f55206
John Allison <john123allison@gmail.com>
2022-12-18 23:12:05 -0500

    write a test for integer literals

--- ast/ast.go
+++ ast/ast.go
@@ -130,3 +130,12 @@ func (i *Identifier) TokenLiteral() string {
 }
 
 func (i *Identifier) String() string { return i.Value }
+
+type IntegerLiteral struct {
+	Token token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
